c96b18e8041d1f3671bcbd4d6d0af83b28418228
ups5000 <ups_5000@hotmail.com>
2019-10-01 16:17:18 +0200

    initial commit

--- app/Models/User_test.php
+++ app/Models/User_test.php
@@ -10,14 +10,14 @@ class User_test extends Model
     /**
      * Update date of birth
      */
-    protected function update_birth($id,$date)
+    public function update_birth($id,$date)
     {
         DB::table('users')
             ->where('id', $id)
             ->update(['birth_date' => $date]);
     }
 
-    protected function get_birth($id){
+    public function get_birth($id){
     	$birth_date = DB::table('users')
     			->select('birth_date')
     			->where('id',$id)
